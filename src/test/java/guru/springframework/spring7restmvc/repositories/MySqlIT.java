b2d6d2997cf0b35cdac91ca6f91cc19dddf53988
Marcus Heisters <marcus.heisters@actaport.de>
2026-08-07 09:58:42 +0200

    section 13: - fix maven config - use maven-failsafe

--- src/test/java/guru/springframework/spring7restmvc/repositories/MySqlIT.java
+++ src/test/java/guru/springframework/spring7restmvc/repositories/MySqlIT.java
@@ -4,12 +4,12 @@ import guru.springframework.spring7restmvc.entities.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.mysql.MySQLContainer;
 
-import javax.sql.DataSource;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,13 +17,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Testcontainers
 @ActiveProfiles("localmysql")
 @SpringBootTest
-public class MySqlTest {
+public class MySqlIT {
 
     @Container
-    private MySQLContainer mySQLContainer = new MySQLContainer("mysql:9");
-
-    @Autowired
-    DataSource dataSource;
+    @ServiceConnection
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:9");
 
     @Autowired
     BeerRepository beerRepository;
